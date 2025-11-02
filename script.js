e5ba506734fa91a1df5a7e1efd190f3f7924d630
4our6ixintel <hytsfx412@gmail.com>
2025-11-02 12:12:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,499 @@
+const matches = {
+    1: [
+      {
+        day: "Day 1",
+        date: "01 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Galaxy FC vs Talex FC", time: "19:00" },
+          { title: "Match 2", teams: "Jamie FC vs Man City FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 2",
+        date: "02 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Liverpool FC vs AFC Gunners FC", time: "19:00" },
+          { title: "Match 2", teams: "Mcdamasen FC vs Man U", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 3",
+        date: "03 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Pafoda FC vs Ten United FC", time: "19:00" },
+          { title: "Match 2", teams: "Galaxy FC vs Man City FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 4",
+        date: "04 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Jamie FC vs Liverpool FC", time: "19:00" },
+          { title: "Match 2", teams: "Talex FC vs AFC Gunners FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 5",
+        date: "05 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Mcdamasen FC vs Pafoda FC", time: "19:00" },
+          { title: "Match 2", teams: "Man U vs Ten United FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 6",
+        date: "06 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Galaxy FC vs Liverpool FC", time: "19:00" },
+          { title: "Match 2", teams: "Jamie FC vs Mcdamasen FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 7",
+        date: "07 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Talex FC vs Pafoda FC", time: "19:00" },
+          { title: "Match 2", teams: "AFC Gunners FC vs Man City FC", time: "19:40" },
+        ],
+      },
+    ],
+  
+    2: [
+      {
+        day: "Day 8",
+        date: "08 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Man U vs Galaxy FC", time: "19:00" },
+          { title: "Match 2", teams: "Ten United FC vs Jamie FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 9",
+        date: "09 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Liverpool FC vs Man City FC", time: "19:00" },
+          { title: "Match 2", teams: "Mcdamasen FC vs AFC Gunners FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 10",
+        date: "10 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Pafoda FC vs Galaxy FC", time: "19:00" },
+          { title: "Match 2", teams: "Talex FC vs Jamie FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 11",
+        date: "11 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Ten United FC vs Liverpool FC", time: "19:00" },
+          { title: "Match 2", teams: "Man U vs AFC Gunners FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 12",
+        date: "12 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Man City FC vs Pafoda FC", time: "19:00" },
+          { title: "Match 2", teams: "Galaxy FC vs Jamie FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 13",
+        date: "13 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Liverpool FC vs Mcdamasen FC", time: "19:00" },
+          { title: "Match 2", teams: "Talex FC vs Man U", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 14",
+        date: "14 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "AFC Gunners FC vs Pafoda FC", time: "19:00" },
+          { title: "Match 2", teams: "Man City FC vs Ten United FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 15",
+        date: "15 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Galaxy FC vs Liverpool FC", time: "19:00" },
+          { title: "Match 2", teams: "Jamie FC vs Talex FC", time: "19:40" },
+        ],
+      },
+    ],
+  
+    3: [
+      {
+        day: "Day 16",
+        date: "16 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Mcdamasen FC vs Man U", time: "19:00" },
+          { title: "Match 2", teams: "Pafoda FC vs AFC Gunners FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 17",
+        date: "17 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Ten United FC vs Man City FC", time: "19:00" },
+          { title: "Match 2", teams: "Galaxy FC vs Mcdamasen FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 18",
+        date: "18 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Jamie FC vs Pafoda FC", time: "19:00" },
+          { title: "Match 2", teams: "Liverpool FC vs Talex FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 19",
+        date: "19 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "AFC Gunners FC vs Galaxy FC", time: "19:00" },
+          { title: "Match 2", teams: "Man U vs Man City FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 20",
+        date: "20 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Ten United FC vs Mcdamasen FC", time: "19:00" },
+          { title: "Match 2", teams: "Jamie FC vs Liverpool FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 21",
+        date: "21 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Talex FC vs Man City FC", time: "19:00" },
+          { title: "Match 2", teams: "Pafoda FC vs Man U", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 22",
+        date: "22 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "AFC Gunners FC vs Ten United FC", time: "19:00" },
+          { title: "Match 2", teams: "Galaxy FC vs Jamie FC", time: "19:40" },
+        ],
+      },
+      {
+        day: "Day 23",
+        date: "23 Dec 2025",
+        games: [
+          { title: "Match 1", teams: "Liverpool FC vs Man U", time: "19:00" },
+          { title: "Match 2", teams: "Mcdamasen FC vs Talex FC", time: "19:40" },
+        ],
+      },
+    ],
+    4: [
+        {
+          day: "Day 24",
+          date: "24 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Pafoda FC vs Galaxy FC", time: "19:00" },
+            { title: "Match 2", teams: "Ten United FC vs Jamie FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 25",
+          date: "25 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Man City FC vs AFC Gunners FC", time: "19:00" },
+            { title: "Match 2", teams: "Man U vs Liverpool FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 26",
+          date: "26 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Talex FC vs Pafoda FC", time: "19:00" },
+            { title: "Match 2", teams: "Galaxy FC vs Man City FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 27",
+          date: "27 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Jamie FC vs AFC Gunners FC", time: "19:00" },
+            { title: "Match 2", teams: "Mcdamasen FC vs Liverpool FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 28",
+          date: "28 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Ten United FC vs Talex FC", time: "19:00" },
+            { title: "Match 2", teams: "Man U vs Pafoda FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 29",
+          date: "29 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Man City FC vs Jamie FC", time: "19:00" },
+            { title: "Match 2", teams: "Galaxy FC vs AFC Gunners FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 30",
+          date: "30 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Liverpool FC vs Ten United FC", time: "19:00" },
+            { title: "Match 2", teams: "Mcdamasen FC vs Man U", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 31",
+          date: "31 Dec 2025",
+          games: [
+            { title: "Match 1", teams: "Pafoda FC vs Man City FC", time: "19:00" },
+            { title: "Match 2", teams: "Talex FC vs Galaxy FC", time: "19:40" },
+          ],
+        },
+      ],
+    
+      5: [
+        {
+          day: "Day 32",
+          date: "01 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "AFC Gunners FC vs Jamie FC", time: "19:00" },
+            { title: "Match 2", teams: "Man U vs Liverpool FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 33",
+          date: "02 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Ten United FC vs Mcdamasen FC", time: "19:00" },
+            { title: "Match 2", teams: "Man City FC vs Talex FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 34",
+          date: "03 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Galaxy FC vs Pafoda FC", time: "19:00" },
+            { title: "Match 2", teams: "Jamie FC vs Liverpool FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 35",
+          date: "04 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "AFC Gunners FC vs Man U", time: "19:00" },
+            { title: "Match 2", teams: "Talex FC vs Liverpool FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 36",
+          date: "05 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Man City FC vs Mcdamasen FC", time: "19:00" },
+            { title: "Match 2", teams: "AFC Gunners FC vs Pafoda FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 37",
+          date: "06 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Man U vs Ten United FC", time: "19:00" },
+            { title: "Match 2", teams: "Jamie FC vs Galaxy FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 38",
+          date: "07 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Talex FC vs Man City FC", time: "19:00" },
+            { title: "Match 2", teams: "Liverpool FC vs AFC Gunners FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 39",
+          date: "08 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Mcdamasen FC vs Pafoda FC", time: "19:00" },
+            { title: "Match 2", teams: "Man U vs Ten United FC", time: "19:40" },
+          ],
+        },
+      ],
+    
+      6: [
+        {
+          day: "Day 40",
+          date: "09 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Galaxy FC vs Liverpool FC", time: "19:00" },
+            { title: "Match 2", teams: "Jamie FC vs Mcdamasen FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 41",
+          date: "10 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Talex FC vs Pafoda FC", time: "19:00" },
+            { title: "Match 2", teams: "AFC Gunners FC vs Man City FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 42",
+          date: "11 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Man U vs Galaxy FC", time: "19:00" },
+            { title: "Match 2", teams: "Ten United FC vs Jamie FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 43",
+          date: "12 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Liverpool FC vs Man City FC", time: "19:00" },
+            { title: "Match 2", teams: "Mcdamasen FC vs AFC Gunners FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 44",
+          date: "13 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Pafoda FC vs Ten United FC", time: "19:00" },
+            { title: "Match 2", teams: "Galaxy FC vs Man City FC", time: "19:40" },
+          ],
+        },
+        {
+          day: "Day 45",
+          date: "14 Jan 2026",
+          games: [
+            { title: "Match 1", teams: "Jamie FC vs Man U", time: "19:00" },
+            { title: "Match 2", teams: "Talex FC vs Liverpool FC", time: "19:40" },
+          ],
+        },
+      ],
+    };    
+  
+  function loadWeek() {
+    const week = document.getElementById("week").value;
+    const container = document.getElementById("match-container");
+    container.innerHTML = "";
+  
+    if (!matches[week] || matches[week].length === 0) {
+      container.innerHTML = `<p style="text-align:center; margin-top:2rem;">No matches scheduled for this week.</p>`;
+      return;
+    }
+  
+    matches[week].forEach(day => {
+      const section = document.createElement("section");
+      section.classList.add("match-day");
+      let html = `<h2>${day.day} — <small>${day.date}</small></h2><div class="projects">`;
+      day.games.forEach(game => {
+        html += `
+          <div class="project-card">
+            <h3>${game.title}</h3>
+            <p>${game.teams}</p>
+            <a href="#">Start time: ${game.time}</a>
+          </div>
+        `;
+      });
+      html += `</div>`;
+      section.innerHTML = html;
+      container.appendChild(section);
+    });
+  }
+  
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  
+  window.onload = loadWeek;
+  
+
+// ==== LOAD WEEK ====
+function loadWeek() {
+  const week = document.getElementById("week").value;
+  const container = document.getElementById("match-container");
+  container.innerHTML = "";
+
+  if (!matches[week] || matches[week].length === 0) {
+    container.innerHTML = `<p style="text-align:center;margin-top:2rem;">No matches scheduled.</p>`;
+    return;
+  }
+
+  matches[week].forEach(day => {
+    const section = document.createElement("section");
+    section.classList.add("match-day");
+    let html = `<h2>${day.day} — <small>${day.date}</small></h2><div class="projects">`;
+
+    day.games.forEach(game => {
+      html += `
+        <div class="project-card" onclick="openModal('${game.title}', '${game.teams}', '${game.time}', '${game.link}')">
+          <h3>${game.title}</h3>
+          <p>${game.teams}</p>
+          <p><strong>Start:</strong> ${game.time}</p>
+        </div>`;
+    });
+
+    html += `</div>`;
+    section.innerHTML = html;
+    container.appendChild(section);
+  });
+}
+
+// ==== MODAL HANDLERS ====
+function openModal(title, teams, time, link) {
+  document.getElementById("modal-title").textContent = title;
+  document.getElementById("modal-teams").textContent = teams;
+  document.getElementById("modal-time").textContent = "Start Time: " + time;
+  document.getElementById("modal-link").href = link;
+
+  startCountdown(time);
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+// ==== COUNTDOWN TIMER ====
+function startCountdown(time) {
+  const countdownEl = document.getElementById("modal-countdown");
+
+  const [hour, minute] = time.split(":").map(Number);
+  const matchTime = new Date();
+  matchTime.setHours(hour, minute, 0);
+
+  function update() {
+    const now = new Date();
+    const diff = matchTime - now;
+    if (diff <= 0) {
+      countdownEl.textContent = "Match is Live!";
+      return;
+    }
+    const hrs = Math.floor(diff / (1000 * 60 * 60));
+    const mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((diff % (1000 * 60)) / 1000);
+    countdownEl.textContent = `Starts in ${hrs}h ${mins}m ${secs}s`;
+    requestAnimationFrame(update);
+  }
+  update();
+}
+
+// ==== SCROLL TO TOP ====
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// ==== THEME TOGGLE ====
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  const btn = document.getElementById("theme-toggle");
+  btn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+}
+
+// ==== INIT ====
+window.onload = loadWeek;
